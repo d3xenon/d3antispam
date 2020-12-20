710eee6838fd5c008232a437e7a51f9cfacf7977
xenon <d3xenon@tafmail.com>
2020-12-20 11:52:59 +0700

    new

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # d3antispam
+
+Пример простого алгоритма, обнаруживающего (и в меру возможностей наказывающего) спам на d3.ru
+
+## Пример использования
+~~~~
+$ export D3USER=YourUsername
+$ export D3PASS='YourPassword'
+$ ./d3antispam.py --period 5h --body 'SPAM DETECTED! https://github.com/d3xenon/d3antispam'
+~~~~
+
+## Ограничения
+- d3antispam не может удалять посты (т.к. мою юзер этого не может)
+- d3 имеет ограничение на количество логина-минусований-комментариев в короткий период времени (d3antispam просто игнорирует ошибку). 
+
+## Протокол работы
+
+https://gist.github.com/d3xenon/7e1cd876f4e1b6b425a936197a14d9ca
---
+++ d3antispam.py
@@ -0,0 +1,291 @@
+#!/usr/bin/python3
+
+import argparse
+import requests
+import json
+import time
+import datetime
+import os
+# from urllib import parse
+from requests.models import PreparedRequest
+
+class d3exc(Exception):
+    pass
+
+class D3():
+    def __init__(self):
+        self.user = None
+        self.password = None
+        self.sid = None
+        self.uid = None
+        self.headers = dict()
+    
+    def session_init(self, sid, uid):
+            self.uid = int(uid)
+            self.sid = sid
+
+            self.headers = {
+                'X-Futuware-UID': str(self.uid),
+                'X-Futuware-SID': self.sid,
+            }
+
+    def auth(self, user=None, password=None):
+        self.user=user
+        self.password=password
+        data = {
+            'username': self.user,
+            'password': self.password
+        }
+
+        r = requests.post('https://d3.ru/api/auth/login/', data=data)
+        
+        if r.status_code == 200:                
+            data = r.json()
+            self.session_init(sid = data['sid'], uid=data['uid'])
+        else:
+            print("auth code:", r.status_code, r.text)
+            raise d3exc(f'{r.status_code} {r.text}')
+
+    def vote(self, post_id, vote):
+        url = f'https://d3.ru/api/posts/{post_id}/vote/'
+        data = {'vote': vote}
+        self.authrequest('POST', url, data)
+
+    def last_posts(self, period, url=None, params=None, user=None):
+        if url is None and params is None:
+            if user:
+                url = f'https://d3.ru/api/users/{user}/posts/'
+                params = {}
+            else:
+                url = 'https://d3.ru/api/posts/'
+                params = {'sorting': 'date_created'}
+
+        page=1
+        
+        while True:
+            reported=0
+            req = PreparedRequest()
+            params['page'] = page
+            req.prepare_url(url, params)
+            # print(req.url)
+
+            r = requests.get(req.url)
+            data = r.json()
+            for pdata in data['posts']:
+                p = Post(pdata, client=self)
+                if p.age() < period:
+                    reported += 1
+                    yield p
+            
+            if not reported:
+                # print(f'// nothing found on page {page} size {len(data["posts"])}')
+                return 
+            else:
+                # print(f'// page {page} size {len(data["posts"])} reported {reported}')
+                page += 1
+
+    def __repr__(self):
+        return f'sid: {self.sid} uid: {self.uid}'
+
+    def authrequest(self, method, url, data=None):
+        if method == 'GET':
+            r = requests.get(url, headers = self.headers)
+        elif method == 'POST':
+            r = requests.post(url, headers=self.headers, data=data)
+        else:
+            raise d3exc(f'unknown method {method}')
+
+        if r.status_code != 200:
+            raise d3exc(f'{method} {url} {r.status_code} {r.text}')
+        return r
+
+class Comment():
+    def __init__(self, post, data):
+        self.data = data
+        self.user = data['user']['login']
+        self.uid = data['user']['id']
+        self.body = data['body']
+        self.post = post
+
+    def __repr__(self):
+        return f'{self.post.id} {self.user}: {self.body}'
+
+
+class Post():
+    def __init__(self, data, client=None):
+        self.data = data
+        self.id = data['id']
+        self.username = data['user']['login']
+        self.title = data['data']['title']
+        self.domain = data['domain']['url']
+        self.rating = data['rating'] or 0
+        self.created = data['created']
+        self.karma = data['user']['karma']
+
+        self.client = client
+    
+
+    def __repr__(self):
+        return f'#{self.id:<10} ({self.age():7}s ago) {self.rating:4} {self.domain:30}{self.username:20}({self.karma}) {self.title}'
+
+    def age(self):
+        return int(time.time() - self.created)
+
+    def dump(self):
+        return json.dumps(self.data, indent=4)
+
+    def vote(self, vote):
+        self.client.vote(self.id, vote)
+
+    def get_comments(self):
+        r = requests.get(f'https://d3.ru/api/posts/{self.id}/comments/')
+        for c in r.json()['comments']:
+            yield Comment(post=self, data=c)
+
+    def comment(self, body):
+        url = f'https://d3.ru/api/posts/{self.id}/comments/'
+        data = {'body': body}
+        return self.client.authrequest('POST', url, data)
+
+        
+
+Posts = [Post]
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description = 'd3 antispam checked')
+    parser.add_argument('--size', type=int, default=20, help='page size (number of posts in page)')
+    parser.add_argument('--period', default='1h', help='period (e.g. 12h or 2d)')
+
+    group = parser.add_argument_group('Authentication')
+    group.add_argument('--user', '-u', help='your username', default = os.getenv('D3USER'))
+    group.add_argument('--password', '-p', help='your password', default=os.getenv('D3PASS'))
+
+    group = parser.add_argument_group('Authentication (debug)')
+    group.add_argument('--uid', help='uid', default = os.getenv('D3UID'))
+    group.add_argument('--sid', help='session id', default=os.getenv('D3SID'))
+
+    group = parser.add_argument_group('Spam criterion')
+    group.add_argument('--posts', type=int, default=10, help='>= N posts during --period')
+    group.add_argument('--neg', type=int, default=5, help='>= N posts with negative rating during --period')
+
+    group = parser.add_argument_group('Spam reaction')
+    group.add_argument('--body', default=None, help='specify body to comment each punished post')
+
+
+    return parser.parse_args()
+
+def get_posts(period: int) -> Posts:
+    sorting = 'date_created'
+    page = 1
+    stop = False
+
+    while not stop:
+        reported = 0
+
+        url = f'https://d3.ru/api/posts/?page={page}&sorting={sorting}'
+        r = requests.get(url)
+        data = r.json()
+        for pdata in data['posts']:
+            p = Post(pdata)
+            if p.age() < period:
+                reported += 1
+                yield p
+        
+        if not reported:
+            print(f'// nothing found on page {page} size {len(data["posts"])}')
+            return 
+        else:
+            print(f'// page {page} size {len(data["posts"])} reported {reported}')
+            page += 1
+
+
+def is_spammer(d3: D3, user: str, period: int, posts: int, neg: int):
+    print(f'checking user {user}...')
+
+    page=1
+    user_karma = 0
+
+    calc_posts = 0
+    calc_neg = 0
+
+    for post in d3.last_posts(period=period, user=user):
+        print(post)
+        user_karma = post.karma
+        if post.age() < period:
+            calc_posts += 1
+            if post.rating < 0:
+                calc_neg += 1
+
+    if calc_posts >= posts and calc_neg >= neg:
+        print(f'User {user} ({user_karma}) is spammer. {calc_posts} posts ({calc_neg} negative) in {period} seconds')
+        return True
+    return False
+
+def punish(d3: D3, user: str, body: str, period: int):
+    for post in d3.last_posts(period=period, user=user):
+            print("PUNISH", post)
+            # VOTE
+            try:
+                post.vote(-1)
+            except d3exc as e:
+                pass
+
+            commented = False
+            # COMMENTED? (unreliable!)
+            for comment in post.get_comments():
+                if comment.uid == d3.uid:
+                    commented = True
+                    break
+                    
+                    
+            if not commented:
+                try:
+                    post.comment(body)
+                except d3exc as e:
+                    pass
+                    # print("comment failed:", e)
+
+# main
+def main():
+    args = get_args()
+
+    users = list()
+
+    if args.period[-1]=='d':
+        period = int(args.period[:-1])*86400
+    elif args.period[-1]=='h':
+        period = int(args.period[:-1])*3600
+    elif args.period[-1]=='s':
+        period = int(args.period[:-1])
+    else:
+        period = int(args.period)
+    
+
+    d3 = D3()
+    if args.uid and args.sid:        
+        d3.session_init(sid=args.sid, uid=args.uid)
+    else:
+        d3.auth(args.user, args.password)
+        print(d3)
+
+    print("Started:", datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+    started = time.time()
+
+    for post in d3.last_posts(period=period):
+        # print(post)
+        if post.rating<0:
+            if not post.username in users:
+                users.append(post.username)
+
+    for user in users:
+        if is_spammer(d3, user, period=period, posts=args.posts, neg=args.neg):
+            punish(d3, user, 
+                body=args.body, 
+                period=period)
+            return
+
+    print(f'Finished in {int(time.time() - started)} seconds')
+
+main()
